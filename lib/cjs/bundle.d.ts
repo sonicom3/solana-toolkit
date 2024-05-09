94121ca0ac7463d36a688625dedeb9a1e5afb3b2
sonicom3 <ladincurshbolt33@gmail.com>
2024-05-09 05:57:53 +0200

    update source

--- lib/cjs/bundle.d.ts
+++ lib/cjs/bundle.d.ts
@@ -1,4 +1,4 @@
 /// <reference types="jito-ts/node_modules/@solana/web3.js" />
 import { Connection, Keypair } from "@solana/web3.js";
-import { BUNDLE_TRANSACTION, SPL_ERROR } from "./global";
-export declare const createAndSendBundleTransaction: (connection: Connection, bundleTip: number, transactions: BUNDLE_TRANSACTION[], payer: Keypair) => boolean;
+import { SPL_ERROR } from "./global";
+export declare const createAndSendBundleTransaction: (connection: Connection, fee: number, bundleTransactions: any, payer: Keypair) => Promise<boolean | SPL_ERROR.E_FAIL>;
